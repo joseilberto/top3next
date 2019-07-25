ef33fcc60c4b601af3efa829cad5acfa39fa0720
José <ilbertofjunior@gmail.com>
2019-07-25 21:38:29 +0200

    Updating data extraction

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from utils import merge_and_index_data
+from utils import get_local_and_users_data, merge_and_index_data
+from models import LSTM_model
+
+
+def train_model(model, data, tokenizer, validation_split = 0.2, epochs = 10):
+    pass
+
+
+
 
 if __name__ == "__main__":
     data_file = "data/kaggle_twitter.csv"
+    model_file = "data/LSTM_model.h5"
     min_tweets = 20
+    local_share = 0.2
     data, tokenizer = merge_and_index_data(data_file, min_tweets)
-    
+    local_data, users_data = get_local_and_users_data(data, local_share)
+    model = LSTM_model(tokenizer.word_index)
+    train_model(model, data, tokenizer)
--- utils.py
+++ utils.py
@@ -1,25 +1,41 @@
 from keras.preprocessing.text import Tokenizer
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
+from random import sample
 
-import nltk
+import numpy as np
 import pandas as pd
 import re
 
+from constants import CLEANING_REGEX, NEGATIONS_DICT
 
-def get_cleaned_text(text, stem = False):
-    cleaning_regex = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
-    stop_words = stopwords.words("english")
-    stemmer = SnowballStemmer("english")
-    text = re.sub(cleaning_regex, " ", str(text).lower()).strip()
+
+def get_cleaned_text(text, stop_words, stemmer, stem = False):    
+    neg_pattern = re.compile(r'\b(' + '|'.join(NEGATIONS_DICT.keys()) + r')\b')    
+    text = re.sub(CLEANING_REGEX, " ", str(text).lower()).strip()
+    text = neg_pattern.sub(lambda x: NEGATIONS_DICT[x.group()], text)
     tokens = []
     for token in text.split():
         if token not in stop_words:
             if stem:
                 tokens.append(stemmer.stem(token))
                 continue
-            tokens.append(token)
-    return " ".join(tokens)
+            tokens.append(token)    
+    text = " ".join(tokens)
+    text = re.sub("n't", "not", text)
+    return re.sub("'s", "is", text)
+
+
+def get_local_and_users_data(data, local_share = 0.1):
+    unique_users = data.user.unique()
+    local_users = np.random.choice(unique_users, 
+                                    int(local_share*unique_users.shape[0]))
+    remote_users = ~np.isin(unique_users, local_users)
+    local_data = data[data.user.isin(local_users)]
+    remote_data = data[data.user.isin(remote_users)]
+    #TODO Finish it later
+
+
 
 
 def index_data_by_date(data, string_tz = "PDT"):
@@ -34,13 +50,16 @@ def index_data_by_date(data, string_tz = "PDT"):
 
 def merge_and_index_data(input_file, min_tweets = 20):
     columns = ["target", "ids", "date", "flag", "user", "text"]
+    stop_words = stopwords.words("english")
+    stemmer = SnowballStemmer("english")
     data = pd.read_csv(input_file, encoding = "ISO-8859-1", header = None, 
                             names = columns)
     data.drop(["target", "flag", "ids"], axis = 1, inplace = True)
     users = data.groupby(by = "user").apply(len) > min_tweets
     data = data[data.user.isin(users[users].index)]
     data = index_data_by_date(data)
-    data.text = data.text.apply(lambda x: get_cleaned_text(x))
+    data.text = data.text.apply(lambda x: get_cleaned_text(x, stop_words, stemmer))
+    data.drop_duplicates(subset = ["text"], keep = False, inplace = True)
     sequences, tokenizer = text_to_sequence(data.text, Tokenizer)
     data["sequence"] = sequences
     data = data[data.sequence.map(lambda x: len(x)) > 0]    
@@ -62,3 +81,5 @@ def text_to_sequence(texts, tokenizer):
     tokenizer = tokenizer()
     tokenizer.fit_on_texts(texts)
     return tokenizer.texts_to_sequences(texts), tokenizer
+
+
