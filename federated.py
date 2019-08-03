057a74f61bab1750aeae938ca23916c7f5896ec9
José <ilbertofjunior@gmail.com>
2019-08-03 18:23:27 +0200

    Next steps on federated part

--- federated.py
+++ federated.py
@@ -1,34 +1,33 @@
-from collections import OrderedDict
-from tensorflow.keras.models import clone_model
-from scipy.special import softmax
-
+import copy
 import numpy as np
-import tensorflow as tf
-import tensorflow_federated as tff
+import syft as sy
+import torch as th
 
 from utils import extend_data
 
 
 def get_federated_dataset(data, users, context_size):
-    users_data = {}
+    users_data = []
+    hook = sy.TorchHook(th)
     for user in users:
+        user_worker = sy.VirtualWorker(hook, id = user)
         cur_data = data[data.user == user]
         X, Y = extend_data(cur_data.X, cur_data.Y, context_size)
-        users_data[user] = {"x": X, "y": Y}
-    return tff.simulation.FromTensorSlicesClientData(users_data)
+        X = th.tensor(X)
+        Y = th.tensor(Y)
+        users_data.append(sy.BaseDataset(X, Y).send(user_worker))
+    return sy.FederatedDataset(users_data)
 
 
-def get_federated_model(model, example_dataset, V, context_size):
-    federated_model = compile_model(clone_model(model), 1e-3)
-    x = tf.constant(np.random.randint(1, V, size = [1, context_size]))
-    y = tf.constant(np.random.randint(1, V, size = [1, 1]))
-    dummy_batch = OrderedDict([('x', x), ('y', y)])
-    return tff.learning.from_compiled_keras_model(federated_model, 
-                                                dummy_batch = dummy_batch)
-
-def train_federated(model, X, Y, model_file, batch_size = 16,
+def train_federated(model, dataloader, model_file, batch_size = 16,
                     epochs = 50, validation_split = 0.1):
-    pass
+    optimizer = th.optim.RMSprop
+    model.fit_generator(dataloader, optimizer, batch_size, epochs,
+                            local = False, validation_split = 0.2,
+                            batch_print_epoch = 2)
+    import ipdb; ipdb.set_trace()
+
+    
 
 
 def train_multiple_federated(model, data, model_file, V, user_split = 0.1, 
@@ -37,18 +36,13 @@ def train_multiple_federated(model, data, model_file, V, user_split = 0.1,
     unique_users = data.user.unique()
     user_batch_size = int(unique_users.shape[0] // (user_split*10**2))
     n_user_batches = int(1 // user_split)
-    model_copy = clone_model(model)
-    model_copy.set_weights(model.get_weights())
+    federated_model = copy.deepcopy(model)    
     for i in range(n_user_batches):
         cur_users = unique_users[i*user_batch_size:(i + 1)*user_batch_size]
         federated_dataset = get_federated_dataset(data, cur_users, context_size)
-        example_dataset = federated_dataset.create_tf_dataset_for_client(
-            federated_dataset.client_ids[0]
-        )
-        federated_average = tff.learning.build_federated_averaging_process(
-            model_fn = lambda: get_federated_model(model_copy, example_dataset, 
-                                V, context_size) 
-        )
-        import ipdb; ipdb.set_trace()
+        federated_loader = sy.FederatedDataLoader(federated_dataset,
+                                    shuffle = False, batch_size = batch_size)
+        federated_model = train_federated(federated_model, federated_loader, 
+                                            model)
 
 
